d7dcefb0af64c1471d6646057874ceceedcef395
nickleon07 <nick.r.leon@gmail.com>
2021-08-28 15:08:21 -0700

    fix comments

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,28 +1,5 @@
 import React, {useState} from 'react';
 
-// =============================================================================================================
-//   Notes:
-//   Props include all relevant information about items in cart.  Based on figma, that would include the following:
-//
-//    Array of ordered meal kits and relevant information:
-//
-//   [ {
-//       meal_id: Number,
-//       mealsPerWeek: Number,
-//       pricePerMeal: Number,
-//     },
-//    ...]
-//
-//    Customer Information object:
-//    {
-//      customerId: Number,
-//      billingAddress: String,
-//      shippingAddress: String,
-//      email: String,
-//      phone: String,
-//    }
-// =============================================================================================================
-
 const Checkout = () => {
   return (
     <>Checkout</>
