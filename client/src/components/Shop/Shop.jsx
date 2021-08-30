f353edad02876ade4cd7164ec7ec53e9509f8df5
Julie Barwick <julie@juliebarwick.com>
2021-08-30 14:54:57 -0700

    Render MealList to Shop component and fetch meals from foodData DB

--- client/src/components/Shop/Shop.jsx
+++ client/src/components/Shop/Shop.jsx
@@ -1,7 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MealList from './MealList.jsx';
+import styled from 'styled-components';
+
+const getMeals = () => axios.get('/meals');
 
 const Shop = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getMeals()
+      .then((data) => {
+        setMeals(data.data.results);
+      })
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log('Error retrieving meals', err);
+      });
+  }, []);
+
   return (
-    <>Shop</>
+    <ShopContainer>
+    <h2>Menu and Pricing</h2>
+    {isLoading
+    ? <p>Loading delicious meals, please be patient...</p>
+    : <MealList meals={meals} />}
+    </ShopContainer>
   );
 };
+
+const ShopContainer = styled.div`
+  background-color: #f8f8f8;
+`;
+export default Shop;
