96925f3ed1827457d6f248f0eb910e3bac6110a1
Sierra Wendt <serigrace@gmail.com>
2021-08-30 16:52:49 -0700

    cancel functionality bare min implemented, put route in place

--- client/src/components/UserProfile/ManageAccount.jsx
+++ client/src/components/UserProfile/ManageAccount.jsx
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import userData from '../../../../database/userData/json/dummyUser1.json';
+import { RiEmotionSadLine, RiEmotionHappyFill } from 'react-icons/ri';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const ManageAccount = () => {
+  const [user, setUser] = useState(userData)
+  const [currentMeal, setMeal] = useState(userData.currentMealPlan);
+
+
+  useEffect(() => {
+    // axios.get('/userdata')
+    //   .then((res) => setMeal(res.data.currentMealPlan))
+    //   .catch((err) => console.log('could not fetch meal plan', err));
+  }, [])
+
+  const cancelPlan = () => {
+    setMeal('');
+
+    // const cancel = {currentMealPlan: null};
+    // axios.put(`${user.id}/currentmealplan`, cancel)
+    //   .then((res) => console.log('Cancel successful', res.status))
+    //   .catch((err) => console.log('Cancel failed', err))
+  }
+
+  console.log(userData);
   return(
     <ManageContainer>
       Current Meal Plan:
       <MealPlanContainer>
         <MealCard>
+          {currentMeal ? <RiEmotionHappyFill /> : <RiEmotionSadLine />}
         </MealCard>
         <ButtonContainer>
-          <button>Cancel</button>
+          <button onClick={cancelPlan}>Cancel</button>
           <button>Modify</button>
         </ButtonContainer>
       </MealPlanContainer>
@@ -35,6 +59,9 @@ const MealPlanContainer = styled.div`
   max-width: 100%; `
 
 const MealCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 2%;
   height: 75%;
   max-width: 30%;
