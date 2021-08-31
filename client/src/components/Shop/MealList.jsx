ec40dd59856a73f1c9f8b5d5b686da3434094b9a
Julie Barwick <julie@juliebarwick.com>
2021-08-30 21:50:31 -0700

    Implement Meals List with filter and slice functionality

--- client/src/components/Shop/MealList.jsx
+++ client/src/components/Shop/MealList.jsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MealCard from './MealCard.jsx';
 
-const MealList = ({ meals }) => {
+const randomNumToNinety = () => Math.floor(Math.random() * 90);
+
+const DIETS = ['Gluten Free', 'Vegetarian', 'Vegan', 'Pescatarian', 'Paleo'];
+const STARTING_IDX = 55;
+const SLICE = 9;
+
+const MealList = ({ meals, loggedIn }) => {
+  const initialMeals = meals.slice(STARTING_IDX, STARTING_IDX + SLICE);
+  const [currentMeals, setCurrentMeals] = useState(initialMeals);
+  const [slicer, setSlicer] = useState(SLICE);
+
+  const handleRegisterOrCheckoutClick = () => {
+    if (loggedIn) {
+      console.log('Go to check out');
+    } else {
+      console.log('Go to registration');
+    }
+  };
+
+  const handleFilterClick = (e) => {
+    const filterText = e.target.name.toLowerCase();
+    const filteredMeals = meals.filter((meal) => meal.diets.some(diet => diet.includes(filterText)));
+    setCurrentMeals(filteredMeals);
+  };
+
+  const resetMeals = () => {
+    setCurrentMeals(initialMeals);
+  };
+
+  const handleAddMoreClick = () => {
+    // TODO: handle when no more meals remaining
+    const newSlice = slicer + SLICE;
+    setCurrentMeals(meals.slice(STARTING_IDX, STARTING_IDX + newSlice));
+    setSlicer(newSlice);
+  }
 
   return (
     <Section>
+      <button onClick={resetMeals}>All Meals</button>
+      {DIETS.map((diet) => (
+        <button
+          key={diet}
+          name={diet}
+          type="button"
+          onClick={handleFilterClick}
+        >
+          {diet}
+        </button>)
+      )}
       <MealsContainer>
-        {meals.map((meal) => (
+        {currentMeals.map((meal) => (
           <MealCard
             key={meal.id}
             id={meal.id}
@@ -17,23 +62,33 @@ const MealList = ({ meals }) => {
           />
         ))}
       </MealsContainer>
+      <div>
+        <button type="button" onClick={handleAddMoreClick}>Show More Meals</button>
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={handleRegisterOrCheckoutClick}
+        >
+        {loggedIn ? 'Checkout' : 'Register'}
+        </button>
+      </div>
     </Section>
   );
-
-}
+};
 
 const MealsContainer = styled.div`
   display: flex;
   box-sizing: border-box;
-  flex: 1 1 0%;
   flex-flow: row wrap;
-  box-sizing: border-box;
+  max-height: 100vh;
+  overflow-y: auto;
 `;
 
-const Section = styled.div`
-  max-width: 960px;
-  padding-left: 0px;
-  padding-right: 0px;
+const Section = styled.section`
+  padding-left: 12.5%;
+  padding-right: 12.5%;
+  margin: 0 auto;
 `;
 
 export default MealList;
