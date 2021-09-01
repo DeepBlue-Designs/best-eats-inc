d03ffc46be1ae8f02e586c4dddc0b4c87b2762b2
Sierra Wendt <serigrace@gmail.com>
2021-09-01 16:15:15 -0700

    resolving merge conflicts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,34 +1,12 @@
-<<<<<<< HEAD
-import React, { useState, useContext, useEffect } from "react";
-import axios from "axios";
-import Header from "./HeaderFooter/Header.jsx";
-import UserProfile from "./UserProfile/UserProfile.jsx";
-=======
 import React, {useState} from 'react';
 import Header from './HeaderFooter/Header.jsx';
 import Context from './Context.jsx';
 
->>>>>>> 9b6763a87cd4e24f1e3174526750fdfae19df726
 
 const App = () => {
-
-
-
   const [userData, setUserData] = useState();
 
   return (
-<<<<<<< HEAD
-    <div>
-      <Header />
-      <h1>
-        /<UserProfile />
-      </h1>
-    </div>
-  );
-};
-
-export default App;
-=======
   <div>
     <Context.Provider value={{userData, setUserData}}>
       <Header />
@@ -38,4 +16,3 @@ export default App;
 };
 
 export default App;
->>>>>>> 9b6763a87cd4e24f1e3174526750fdfae19df726
