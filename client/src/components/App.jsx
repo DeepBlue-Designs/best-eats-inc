bb0837f61735e3ab58ef16752fb64d10a3f72421
Sierra Wendt <serigrace@gmail.com>
2021-09-01 19:58:50 -0700

    created global style component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import GlobalStyle from './GlobalStyle.jsx';
 import Header from './HeaderFooter/Header.jsx';
 import Context from './Context.jsx';
 
@@ -8,6 +9,8 @@ const App = () => {
 
   return (
   <div>
+    {/* GLOBAL STYLE COMMENTED OUT FOR SANITY */}
+    {/* <GlobalStyle /> */}
     <Context.Provider value={{userData, setUserData}}>
       <Header />
     </Context.Provider>
