2e3435bde4ecdeff7a28a365bfcc15934e6569d1
Sierra Wendt <serigrace@gmail.com>
2021-09-02 21:02:37 -0700

    manage account styling

--- client/src/components/UserProfile/ManageAccount.jsx
+++ client/src/components/UserProfile/ManageAccount.jsx
@@ -19,18 +19,19 @@ const ManageAccount = () => {
 
   return(
     <ManageContainer>
-      <Text>
-      Current Meal Plan:
-      </Text>
       <MealPlanContainer>
         <MealCard>
+        <Text>
+      Current Meal Plan:
+      </Text>
         {currentMeal && currentMeal.mealIDs.length ? <MealPlanCard plan={currentMeal.plan} mealsPerWeek={currentMeal.mealsPerWeek} style={{'background-color': '#BAFFAE'}}/> : "Please Select a MealPlan" }
         </MealCard>
         <ButtonContainer>
           <button onClick={cancelPlan}
-          style={{'border': '5px solid #baffae', 'padding':'14px 57px', 'background-color': 'white', 'width': 'fit-content'}}>Cancel</button>
+          style={{'border': '5px solid #baffae', 'padding':'14px 57px', 'background-color': '#baffae', 'width': 'fit-content', 'box-shadow': 'rgb(0 0 0 / 15%) 0px 2px 6px 0px'
+        }}>Cancel</button>
             <Link to="/shop">
-              <button style={{'border': '5px solid #BAFFAE', 'padding': '14px 57px', 'background-color': 'white'}}>Modify</button>
+              <button style={{'border': '5px solid #BAFFAE', 'padding': '14px 57px', 'background-color': '#baffae','box-shadow': 'rgb(0 0 0 / 15%) 0px 2px 6px 0px'}}>Modify</button>
             </Link>
         </ButtonContainer>
       </MealPlanContainer>
@@ -44,27 +45,29 @@ const ManageContainer = styled.div`
   display: flex;
   margin-bottom: 2%;
   flex-direction: column;
-  border: 1px solid lightblue;
+  border-radius: 5px;
+  box-shadow: rgb(0 0 0 / 15%) 0px 2px 6px 0px;
   `
 
 const MealPlanContainer = styled.div`
   display: flex;
   flex-direction: row;
-  /* border: 1px solid orange; */
   height: 500px;
   justify-content: space-evenly;
-  align-items: baseline;
+  align-items: center;
   max-width: 100%; `
 
 const MealCard = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 2%;
   height: 75%;
-  max-width: 20%;
-  min-width: 30%;
-  background-color: #baffae;
+  width: 300px;
+  background-color: rgb(186 255 174 / 24%);
+  border: 5px solid #BAFFAE;
+  box-shadow: rgb(0 0 0 / 15%) 0px 2px 6px 0px;
   border-radius: 5px;
   `
 
@@ -80,5 +83,7 @@ const ButtonContainer = styled.div`
 `
 
 const Text = styled.div`
-  padding: 29px 75px 0px;
+  padding: 6px 20px;
+  align-self: flex-start;
+  margin-bottom: 5%;
 `
